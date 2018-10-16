d1def04eb6604e9de46a6f047ebee55ecea03903
iain barnett <iainspeed@gmail.com>
2018-10-16 15:15:00 +0900

    - Big increase in number of specs. - Tightened up checks for valid arguments. - Made some of the method calls of some other libraries' valid

--- lib/ansi_colors.rb
+++ lib/ansi_colors.rb
@@ -5,6 +5,46 @@ module AnsiColors
   class Error < StandardError; end
 
 
+  # Mimic Term AnsiColor methods too.
+  BACKGROUNDS = {
+    :black            =>    [40, :default_background],
+    :red              =>    [41, :default_background],
+    :green            =>    [42, :default_background],
+    :yellow           =>    [43, :default_background],
+    :blue             =>    [44, :default_background],
+    :magenta          =>    [45, :default_background],
+    :cyan             =>    [46, :default_background],
+    :white            =>    [47, :default_background],
+  }.each_with_object({}) {|(k,v),hs|
+    hs["bkc_#{k}".to_sym] = v
+    hs["on_#{k}".to_sym] = v
+  }
+
+
+  DEFAULT_COLORS = {
+    :black                =>    30,
+    :red                  =>    31,
+    :green                =>    32,
+    :yellow               =>    33,
+    :blue                 =>    34,
+    :magenta              =>    35,
+    :cyan                 =>    36,
+    :white                =>    37,
+  }
+
+
+  DEFAULT_COLOR_CODES = {
+    30  =>  :black,
+    31  =>  :red,
+    32  =>  :green,
+    33  =>  :yellow,
+    34  =>  :blue,
+    35  =>  :magenta,
+    36  =>  :cyan,
+    37  =>  :white
+  }
+
+
   # The color commands understood by the overridden method_missing method.
   ANSI_CMDS = {
     :bright               =>    [1, :normal],
@@ -17,65 +57,56 @@ module AnsiColors
     :inverse              =>    [7, 27],
     :conceal              =>    [8, 28],
     :crossed_out          =>    [9, 29],
-    :black                =>    [30, :default_color],
-    :red                  =>    [31, :default_color],
-    :green                =>    [32, :default_color],
-    :yellow               =>    [33, :default_color],
-    :blue                 =>    [34, :default_color],
-    :magenta              =>    [35, :default_color],
-    :cyan                 =>    [36, :default_color],
-    :white                =>    [37, :default_color],
-    :bck_black            =>    [40, :default_background],
-    :bck_red              =>    [41, :default_background],
-    :bck_green            =>    [42, :default_background],
-    :bck_yellow           =>    [43, :default_background],
-    :bck_blue             =>    [44, :default_background],
-    :bck_magenta          =>    [45, :default_background],
-    :bck_cyan             =>    [46, :default_background],
-    :bck_white            =>    [47, :default_background],
     :reset                =>    0,
     :normal               =>    22,
     :default_color        =>    39,
     :default_background   =>    49		
-  }
+  }.merge( BACKGROUNDS )
+    .merge( DEFAULT_COLORS.each_with_object({}) {|(k,v), hs|
+      hs[k] = [v,:default_color]
+    })
 
 
-  class << self
-
-    # Adds escape chars to the string so that it appears coloured when printed on an
-    # ansi compliant terminal (non-ansi terminals will display the string surrounded
-    # by garbage-like stuff). col_code specifies the color to be used. 
-    def colorize(str, col_code, end_code)
-      "\033[#{col_code}m#{str}\033[#{end_code}m"
+  # Adds escape chars to the string so that it appears
+  # colored when printed on an ansi compliant terminal
+  # (non-ansi terminals will display the string surrounded
+  # by garbage-like stuff). col_code specifies the color to be used. 
+  def colorize(color_code, end_code=nil)
+    if color_code.respond_to? :integer?
+      color_code = DEFAULT_COLOR_CODES[color_code]
     end
-
-
-    def get_codes meth
-      color_code = ANSI_CMDS[meth.to_s[5..-1].to_sym]
-      end_code = :reset
-
-      if color_code.responds_to? :each_index
-        color_code, end_code = color_code
-      end
-
-      if end_code.is_a? Symbol
-        end_code = ANSI_CMDS[end_code]
-      end
-
-      [color_code,end_code]
+    if end_code.nil? or color_code.nil?
+      color_code, end_code = get_codes color_code
     end
-  
+    "\033[#{color_code}m#{self}\033[#{end_code}m"
   end
 
 
-  def colorize col_code, end_code
-    self.class.colorize self, col_code, end_code
+  # @private
+  def get_codes meth
+    fail AnsiColors::Error, "Can't supply nil as a code or a way to get a code" if meth.nil?
+    unless color_code = ANSI_CMDS[meth.to_sym]
+      fail AnsiColors::Error, "There is no color/method specified with that name or code"
+    end
+
+    end_code = :reset
+
+    if color_code.respond_to? :each_index
+      color_code, end_code = color_code
+    end
+
+    if end_code.is_a? Symbol
+      end_code = ANSI_CMDS[end_code]
+    end
+
+    [color_code,end_code]
   end
 
 
+  # Make convenience methods.
   ANSI_CMDS.each do |k,v|
     define_method "ansi_#{k}" do
-      colorize *get_codes(meth)
+      colorize k
     end
   end
 
--- spec/ansi_colors_spec.rb
+++ spec/ansi_colors_spec.rb
@@ -16,4 +16,182 @@ context "Mixing" do
   AnsiColors::ANSI_CMDS.each do |k,v|
     And { mixed_instance.respond_to? "ansi_#{k}".to_sym }
   end
+
+  context "The codes" do
+    context "Valid codes" do
+      AnsiColors::ANSI_CMDS.each do |k,v|
+        context k.to_s do
+          When(:code) { mixed_instance.get_codes k }
+          Then { code.first == v or code.first == v.first }
+        end
+      end
+
+      context "30, black" do
+        When(:coloured) { mixed_instance.colorize 30 }
+        Then { coloured == "\e[30mHello\e[39m" }
+      end
+    end
+
+    context "Invalid codes" do
+      context "Given a colour not in the list" do
+        it "should raise an error" do
+          expect { mixed_instance.colorize :orange }.to raise_error AnsiColors::Error
+        end
+      end
+      context "Given a number that is not valid" do
+        it "should raise an error" do
+          expect { mixed_instance.colorize 33_000_000 }.to raise_error AnsiColors::Error
+        end
+      end
+    end
+  end
+
+  
+  context "Calling colours directly" do
+    context "reset" do
+      When(:coloured) { mixed_instance.ansi_reset }
+      Then { coloured == "\e[0mHello\e[0m" }
+    end
+    context "bold" do
+      When(:coloured) { mixed_instance.ansi_bold }
+      Then { coloured == "\e[1mHello\e[22m" }
+    end
+    context "underline" do
+      When(:coloured) { mixed_instance.ansi_underline }
+      Then { coloured == "\e[4mHello\e[24m" }
+    end
+
+    context "black" do
+      When(:coloured) { mixed_instance.ansi_black }
+      Then { coloured == "\e[30mHello\e[39m" }
+    end
+    context "red" do
+      When(:coloured) { mixed_instance.ansi_red }
+      Then { coloured == "\e[31mHello\e[39m" }
+    end
+    context "green" do
+      When(:coloured) { mixed_instance.ansi_green }
+      Then { coloured == "\e[32mHello\e[39m" }
+    end
+    context "yellow" do
+      When(:coloured) { mixed_instance.ansi_yellow }
+      Then { coloured == "\e[33mHello\e[39m" }
+    end
+    context "blue" do
+      When(:coloured) { mixed_instance.ansi_blue }
+      Then { coloured == "\e[34mHello\e[39m" }
+    end
+    context "magenta" do
+      When(:coloured) { mixed_instance.ansi_magenta }
+      Then { coloured == "\e[35mHello\e[39m" }
+    end
+    context "cyan" do
+      When(:coloured) { mixed_instance.ansi_cyan }
+      Then { coloured == "\e[36mHello\e[39m" }
+    end
+    context "white" do
+      When(:coloured) { mixed_instance.ansi_white }
+      Then { coloured == "\e[37mHello\e[39m" }
+    end
+
+    context "on_black" do
+      When(:coloured) { mixed_instance.ansi_on_black }
+      Then { coloured == "\e[40mHello\e[49m" }
+    end
+    context "on_red" do
+      When(:coloured) { mixed_instance.ansi_on_red }
+      Then { coloured == "\e[41mHello\e[49m" }
+    end
+    context "on_green" do
+      When(:coloured) { mixed_instance.ansi_on_green }
+      Then { coloured == "\e[42mHello\e[49m" }
+    end
+    context "on_yellow" do
+      When(:coloured) { mixed_instance.ansi_on_yellow }
+      Then { coloured == "\e[43mHello\e[49m" }
+    end
+    context "on_blue" do
+      When(:coloured) { mixed_instance.ansi_on_blue }
+      Then { coloured == "\e[44mHello\e[49m" }
+    end
+    context "on_magenta" do
+      When(:coloured) { mixed_instance.ansi_on_magenta }
+      Then { coloured == "\e[45mHello\e[49m" }
+    end
+    context "on_cyan" do
+      When(:coloured) { mixed_instance.ansi_on_cyan }
+      Then { coloured == "\e[46mHello\e[49m" }
+    end
+    context "on_white" do
+      When(:coloured) { mixed_instance.ansi_on_white }
+      Then { coloured == "\e[47mHello\e[49m" }
+    end
+
+    context "black" do
+      When(:coloured) { mixed_instance.ansi_black }
+      Then { coloured == "\e[30mHello\e[39m" }
+    end
+    context "red" do
+      When(:coloured) { mixed_instance.ansi_red }
+      Then { coloured == "\e[31mHello\e[39m" }
+    end
+    context "green" do
+      When(:coloured) { mixed_instance.ansi_green }
+      Then { coloured == "\e[32mHello\e[39m" }
+    end
+    context "yellow" do
+      When(:coloured) { mixed_instance.ansi_yellow }
+      Then { coloured == "\e[33mHello\e[39m" }
+    end
+    context "blue" do
+      When(:coloured) { mixed_instance.ansi_blue }
+      Then { coloured == "\e[34mHello\e[39m" }
+    end
+    context "magenta" do
+      When(:coloured) { mixed_instance.ansi_magenta }
+      Then { coloured == "\e[35mHello\e[39m" }
+    end
+    context "cyan" do
+      When(:coloured) { mixed_instance.ansi_cyan }
+      Then { coloured == "\e[36mHello\e[39m" }
+    end
+    context "white" do
+      When(:coloured) { mixed_instance.ansi_white }
+      Then { coloured == "\e[37mHello\e[39m" }
+    end
+
+    context "Background only bkc_black" do
+      When(:coloured) { mixed_instance.ansi_bkc_black }
+      Then { coloured == "\e[40mHello\e[49m" }
+    end
+    context "Background only bkc_red" do
+      When(:coloured) { mixed_instance.ansi_bkc_red }
+      Then { coloured == "\e[41mHello\e[49m" }
+    end
+    context "Background only bkc_green" do
+      When(:coloured) { mixed_instance.ansi_bkc_green }
+      Then { coloured == "\e[42mHello\e[49m" }
+    end
+    context "Background only bkc_yellow" do
+      When(:coloured) { mixed_instance.ansi_bkc_yellow }
+      Then { coloured == "\e[43mHello\e[49m" }
+    end
+    context "Background only bkc_blue" do
+      When(:coloured) { mixed_instance.ansi_bkc_blue }
+      Then { coloured == "\e[44mHello\e[49m" }
+    end
+    context "Background only bkc_magenta" do
+      When(:coloured) { mixed_instance.ansi_bkc_magenta }
+      Then { coloured == "\e[45mHello\e[49m" }
+    end
+    context "Background only bkc_cyan" do
+      When(:coloured) { mixed_instance.ansi_bkc_cyan }
+      Then { coloured == "\e[46mHello\e[49m" }
+    end
+    context "Background only bkc_white" do
+      When(:coloured) { mixed_instance.ansi_bkc_white }
+      Then { coloured == "\e[47mHello\e[49m" }
+    end
+  end
+
 end
